4ca3adb49ba81aafef408c9f17b8e6e1dff98adf
Mark Salam <mark.salam@gmail.com>
2025-03-23 07:45:18 -0400

    site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://marksalam.com',
     integrations: [
         mdx(),
         sitemap(),
